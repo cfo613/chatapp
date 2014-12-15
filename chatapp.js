8822efccc87f7b7f5c8d58b3d9bb25938231d7a1
Ciara Foran <Ciara@Ciaras-MacBook-Air.local>
2014-12-15 10:13:38 -0500

    updated address

--- chatapp.js
+++ chatapp.js
@@ -1,5 +1,5 @@
 //create WebSocket
-var client = new WebSocket("http://ciara.princesspeach.nyc:3000");
+var client = new WebSocket("ws://ciara.princesspeach.nyc:3000");
 
 client.addEventListener("open", function(evt) {
 
